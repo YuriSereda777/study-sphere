ef7fde00823560dd5e9b82c464259171c46ce9e5
YuriSereda777 <lomaj3ch@hotmail.com>
2023-08-07 00:00:00 +0300

    created navLinks.js data file

---
+++ src/data/navLinks.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { id: "about", value: "About" },
+  { id: "courses", value: "Courses" },
+  { id: "teachers", value: "Teachers" },
+  { id: "contact", value: "Contact" },
+];
