bf835574d65739e2c81c985d38c29a8f759461e0
YuriSereda777 <lomaj3ch@hotmail.com>
2023-08-07 00:00:00 +0300

    updated Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,28 @@
+import { Flex, ListItem, UnorderedList } from "@chakra-ui/react";
+import { navLinks } from "../data/navLinks";
+import logo from "../assets/logo.png";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Flex
+      as="nav"
+      py="10px"
+      alignItems="center"
+      justifyContent="space-around"
+      backgroundColor="white"
+    >
+      <img src={logo} height="100px" />
+      <UnorderedList
+        sx={{ display: "flex", gap: "15px", listStyle: "none" }}
+        fontSize="20px"
+      >
+        {navLinks.map((link) => (
+          <ListItem key={link.id}>
+            <a href={`#${link.id}`}>{link.value}</a>
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </Flex>
   );
 };
 
