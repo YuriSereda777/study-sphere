1e5f285f9f93a24dcd28e76d53b67a4daff3aef4
YuriSereda777 <lomaj3ch@hotmail.com>
2023-08-12 00:00:00 +0300

    created teachers.js for teachers data

---
+++ src/data/teachers.js
@@ -0,0 +1,82 @@
+export const teachers = [
+  {
+    id: 1,
+    firstName: "Walter",
+    lastName: "White",
+    title: "Chemistry Teacher",
+    description:
+      "Passionate about making complex concepts simple and engaging for students.",
+  },
+  {
+    id: 2,
+    firstName: "Daniel",
+    lastName: "Smith",
+    title: "Mathematics Instructor",
+    description:
+      "Excels in creating a positive learning environment and fostering critical thinking skills.",
+  },
+  {
+    id: 3,
+    firstName: "Sophia",
+    lastName: "Davis",
+    title: "Physics Professor",
+    description:
+      "Inspires curiosity and creativity in the study of the physical world.",
+  },
+  {
+    id: 4,
+    firstName: "Michael",
+    lastName: "Johnson",
+    title: "History Educator",
+    description:
+      "Bringing history to life through captivating storytelling and interactive lessons.",
+  },
+  {
+    id: 5,
+    firstName: "Olivia",
+    lastName: "Anderson",
+    title: "English Literature Teacher",
+    description:
+      "Passionate about literature and fostering a love for reading and critical analysis.",
+  },
+  {
+    id: 6,
+    firstName: "Matthew",
+    lastName: "Garcia",
+    title: "Computer Science Instructor",
+    description:
+      "Guiding students through the fascinating world of algorithms and programming.",
+  },
+  {
+    id: 7,
+    firstName: "Ava",
+    lastName: "Parisian",
+    title: "Art and Design Professor",
+    description:
+      "Encouraging creativity and self-expression through various artistic mediums.",
+  },
+  {
+    id: 8,
+    firstName: "William",
+    lastName: "Miller",
+    title: "Physical Education Coach",
+    description:
+      "Promoting a healthy and active lifestyle through fun and inclusive physical activities.",
+  },
+  {
+    id: 9,
+    firstName: "Sophie",
+    lastName: "Turner",
+    title: "Music Teacher",
+    description:
+      "Fostering a deep appreciation for music and guiding students on their musical journey.",
+  },
+  {
+    id: 10,
+    firstName: "Benjamin",
+    lastName: "Clark",
+    title: "Geography Professor",
+    description:
+      "Making geography exciting by connecting it to real-world events and global issues.",
+  },
+];
