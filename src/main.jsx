6128ee3fd4f706d04f2416505897e678acda6b14
YuriSereda777 <lomaj3ch@hotmail.com>
2023-11-14 11:21:50 +0200

    updated chakra ui breakpoints

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
 
+const breakpoints = {
+  base: "0px",
+  sm: "480px",
+  md: "768px",
+  lg: "976px",
+  xl: "1440px",
+};
+
+const theme = extendTheme({ breakpoints });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>
 );
