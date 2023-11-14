ea54d400fb635604670edaf16c0ecc3c4d11432e
YuriSereda777 <lomaj3ch@hotmail.com>
2023-11-14 18:01:03 +0200

    added all the sections to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Box } from "@chakra-ui/react";
+import Hero from "./components/Hero";
+import Navbar from "./layout/Navbar";
+import About from "./components/About";
+import Courses from "./components/Courses";
+import Contact from "./components/Contact";
+import Teachers from "./components/Teachers";
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Box as="div" minHeight="100vh" backgroundColor="gray.50">
+      <Navbar />
+      <Hero />
+      <About />
+      <Courses />
+      <Teachers />
+      <Contact />
+    </Box>
   );
 };
 
