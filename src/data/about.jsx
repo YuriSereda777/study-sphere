4564217ab8ef05c68c83878335f9b345f3016e85
YuriSereda777 <lomaj3ch@hotmail.com>
2023-11-14 12:52:42 +0200

    about data jsx file

---
+++ src/data/about.jsx
@@ -0,0 +1,21 @@
+import onlineLearning from "../assets/online-learning.svg";
+import educatorGuidance from "../assets/educator-guidance.svg";
+
+export const about = {
+  intro:
+    "StudySphere, a vibrant educational hub, is committed to fostering a dynamic and inclusive academic environment. We inspire students to explore their potential, engage in meaningful connections, and achieve excellence in education and personal development.",
+  sections: [
+    {
+      id: 1,
+      title: "Empowering Education, Anytime, Anywhere",
+      text: "StudySphere: Where education knows no limits. Our online platform brings world-class learning to you, offering interactive lessons, collaborative tools, and cutting-edge resources. Join us in shaping the future of digital education.",
+      image: onlineLearning,
+    },
+    {
+      id: 1,
+      title: "Guided Learning with Expert Instructors",
+      text: "Experience personalized learning at StudySphere with dedicated educators guiding you through an enriching journey of knowledge. Benefit from expert guidance, interactive lessons, and a supportive learning community to excel in your academic pursuits.",
+      image: educatorGuidance,
+    },
+  ],
+};
