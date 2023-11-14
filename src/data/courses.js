120fbe3a336078a981531c6b9a08e3bb0e56a569
YuriSereda777 <lomaj3ch@hotmail.com>
2023-11-14 17:59:56 +0200

    courses data file

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,11 +1,80 @@
-const courses = [
+export const courses = [
   {
     id: 1,
-    title: "",
+    title: "Learn Python – Interactive Python",
+    image:
+      "https://themephi.net/template/eduan/eduan/assets/img/course/3/1.jpg",
     lessons: 48,
     hours: 20,
     students: 50,
-    instructorName: "",
-    instructorImage: "",
-  }
-]
+    instructorName: "Hanson Lindgren",
+    instructorImage:
+      "https://themephi.net/template/eduan/eduan/assets/img/testimonial/3/2.jpg",
+  },
+  {
+    id: 2,
+    title: "Web Development Bootcamp",
+    image:
+      "https://themephi.net/template/eduan/eduan/assets/img/course/3/2.jpg",
+    lessons: 60,
+    hours: 30,
+    students: 80,
+    instructorName: "Emily Rodriguez",
+    instructorImage: "https://example.com/instructor2.jpg",
+  },
+  {
+    id: 3,
+    title: "Introduction to Data Science",
+    image:
+      "https://themephi.net/template/eduan/eduan/assets/img/course/3/3.jpg",
+    lessons: 45,
+    hours: 25,
+    students: 60,
+    instructorName: "Alex Turner",
+    instructorImage: "https://example.com/instructor3.jpg",
+  },
+  {
+    id: 4,
+    title: "Graphic Design Fundamentals",
+    image:
+      "https://ivy-school.thimpress.com/demo-4/wp-content/uploads/sites/4/2017/12/Untitled-19-450x300.jpg",
+    lessons: 35,
+    hours: 18,
+    students: 40,
+    instructorName: "Olivia Martinez",
+    instructorImage: "https://example.com/instructor4.jpg",
+  },
+  {
+    id: 5,
+    title: "Mobile App Development",
+    image:
+      "https://ivy-school.thimpress.com/demo-4/wp-content/uploads/sites/4/2017/12/Untitled-18-450x300.jpg",
+    lessons: 55,
+    hours: 28,
+    students: 70,
+    instructorName: "Daniel Baker",
+    instructorImage: "https://example.com/instructor5.jpg",
+  },
+  {
+    id: 6,
+    title: "Photography Basics",
+    image:
+      "https://ivy-school.thimpress.com/demo-4/wp-content/uploads/sites/4/2017/12/Untitled-15-450x300.jpg",
+    lessons: 40,
+    hours: 22,
+    students: 55,
+    instructorName: "Sophia Johnson",
+    instructorImage: "https://example.com/instructor6.jpg",
+  },
+  {
+    id: 7,
+    title: "Social Media Marketing Strategies",
+    image:
+      "https://ivy-school.thimpress.com/demo-4/wp-content/uploads/sites/4/2018/10/Untitled-1l-1-450x300.jpg",
+    lessons: 50,
+    hours: 24,
+    students: 65,
+    instructorName: "Mason White",
+    instructorImage: "https://example.com/instructor7.jpg",
+  },
+];
