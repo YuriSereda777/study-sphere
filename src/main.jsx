858018ccd37881e1d8f9e373877b6e0bb351afed
YuriSereda777 <lomaj3ch@hotmail.com>
2023-11-15 15:58:40 +0200

    added chakra brand colors

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,22 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
 
-const breakpoints = {
-  base: "0px",
-  sm: "480px",
-  md: "768px",
-  lg: "976px",
-  xl: "1440px",
-};
-
-const theme = extendTheme({ breakpoints });
+const theme = extendTheme({
+  breakpoints: {
+    base: "0px",
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
+  colors: {
+    brand: {
+      100: "#867fec",
+      500: "#7971ea",
+      900: "#6d66d3",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider theme={theme}>
